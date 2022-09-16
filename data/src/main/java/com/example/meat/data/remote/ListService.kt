45f719dccc81b6586a560992c85d3b48fae63a60
양윤재 <diddbswo04@dgsw.hs.kr>
2022-09-16 14:00:56 +0900

    [ADD] Retrofit List Service 추가

---
+++ data/src/main/java/com/example/meat/data/remote/ListService.kt
@@ -0,0 +1,10 @@
+package com.example.meat.data.remote
+
+import com.example.meat.data.entity.ListData
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface ListService {
+    @GET("/products")
+    fun getList(): Response<ListData>
+}
