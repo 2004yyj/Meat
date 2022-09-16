67d5b58bb73b47a6bb251059573188d073863207
양윤재 <diddbswo04@dgsw.hs.kr>
2022-09-16 15:11:32 +0900

    [TEST] ListRepository 테스트 성공

---
+++ data/src/test/java/com/example/meat/data/repository/ListRepositoryTest.kt
@@ -0,0 +1,87 @@
+package com.example.meat.data.repository
+
+import com.example.meat.data.repository.fake.FakeListDataSource
+import com.example.meat.domain.model.Category
+import com.example.meat.domain.model.Product
+import com.example.meat.domain.repository.ListRepository
+import junit.framework.Assert
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+class ListRepositoryTest {
+    private lateinit var listDataSource: FakeListDataSource
+    private lateinit var listRepository: ListRepository
+
+    @Before
+    fun before() {
+        listDataSource = FakeListDataSource()
+        listRepository = ListRepositoryImpl(listDataSource)
+    }
+
+    @Test
+    fun 카테고리_가져오기() = runTest {
+        val expected = listOf(
+            Category("pork", "돼지", 0),
+            Category("beef", "소", 1),
+            Category("chicken", "닭", 2)
+        )
+        var actual: List<Category>? = null
+        val testJob = launch(UnconfinedTestDispatcher()) {
+            listRepository.getCategory().collect {
+                actual = it
+            }
+        }
+        Assert.assertEquals(expected, actual)
+        testJob.cancel()
+    }
+
+    @Test
+    fun 카테고리별_상품_가져오기() = runTest {
+        val expected = listOf(
+            Product(
+                "pork01",
+                "pork",
+                "삼겹살",
+                19800,
+                "https://android-test.yookgak.com/static/JeongyookgakLogo.png",
+                0
+            ),
+
+            Product(
+                "pork02",
+                "pork",
+                "목살",
+                18600,
+                "https://android-test.yookgak.com/static/JeongyookgakLogo.png",
+                1
+            ),
+            Product(
+                "pork03",
+                "pork",
+                "항정살",
+                19800,
+                "https://android-test.yookgak.com/static/JeongyookgakLogo.png",
+                2
+            ),
+            Product(
+                "pork04",
+                "pork",
+                "등갈비",
+                19800,
+                "https://android-test.yookgak.com/static/JeongyookgakLogo.png",
+                3
+            )
+        )
+        var actual: List<Product>? = null
+        val testJob = launch(UnconfinedTestDispatcher()) {
+            listRepository.getProductByCategory("pork").collect {
+                actual = it
+            }
+        }
+        Assert.assertEquals(expected, actual)
+        testJob.cancel()
+    }
+}
