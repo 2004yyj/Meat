7b8cd5fc3cf11c75ada1d51ebfd71cb6832b105d
양윤재 <diddbswo04@dgsw.hs.kr>
2022-09-16 20:08:44 +0900

    [FIX] 프리뷰 코드 제거

--- presentation/src/main/java/com/example/meat/screen/home/list/Product.kt
+++ presentation/src/main/java/com/example/meat/screen/home/list/Product.kt
@@ -1,21 +1,24 @@
 package com.example.meat.screen.home.list
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.*
+import androidx.compose.material.ScrollableTabRow
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRowDefaults
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.meat.screen.home.list.key.ProductByKey
-import com.google.accompanist.pager.*
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalPagerApi::class)
@@ -61,10 +64,4 @@ fun Product(
             }
         }
     }
-}
-
-@Preview
-@Composable
-fun ProductPreview() {
-    Product()
 }
