0a204260c8f674199a2bfb73d2cfd9ac733275f0
양윤재 <diddbswo04@dgsw.hs.kr>
2022-09-16 17:51:46 +0900

    [FIX] CompileSdk 변경

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -1,5 +1,5 @@
 object Config {
-    const val compileSdk = 32
+    const val compileSdk = 33
     const val applicationId = "com.example.meat"
     const val minSdk = 24
     const val targetSdk = 32
