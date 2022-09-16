b13fb79c81014666d91814810951b7485bd80e9a
양윤재 <diddbswo04@dgsw.hs.kr>
2022-09-16 11:20:23 +0900

    [ADD] 화면 state 변수 지정

--- presentation/src/main/java/com/example/meat/screen/App.kt
+++ presentation/src/main/java/com/example/meat/screen/App.kt
@@ -5,10 +5,20 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.meat.screen.detail.Detail
+import com.example.meat.screen.home.Home
 import com.example.meat.ui.theme.MeatTheme
 
 @Composable
-fun App() {
+fun App(
+    appState: AppState = rememberAppState()
+) {
     MeatTheme {
         // A surface container using the 'background' color from the theme
         Surface(
