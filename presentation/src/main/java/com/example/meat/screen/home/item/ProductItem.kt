19c27f5b047c6b2f0c5d3e89f7d2ee5a25dadf82
양윤재 <diddbswo04@dgsw.hs.kr>
2022-09-17 12:16:01 +0900

    [ADD] 상품 아이템에 가격 및 좋아요 체크박스 추가

--- presentation/src/main/java/com/example/meat/screen/home/item/ProductItem.kt
+++ presentation/src/main/java/com/example/meat/screen/home/item/ProductItem.kt
@@ -2,13 +2,15 @@ package com.example.meat.screen.home.item
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import com.example.meat.components.checkbox.HeartCheckbox
 import com.example.meat.domain.model.Product
+import com.example.meat.theme.Typography
 
 @Composable
 fun ProductItem(
@@ -16,19 +18,39 @@ fun ProductItem(
     product: Product,
     onClick: (Product) -> Unit = {},
 ) {
+    var checked by remember { mutableStateOf(false) }
     Row(
-        modifier = modifier.fillMaxWidth()
+        modifier = modifier
+            .fillMaxWidth()
+            .height(ItemSize)
     ) {
         AsyncImage(
-            modifier = Modifier.size(80.dp),
+            modifier = Modifier.size(ItemSize),
             model = product.thumbnail,
-            contentDescription = "ProductThumbnail"
+            contentDescription = "ProductThumbnail",
         )
 
-        Box(modifier = modifier.weight(1f)) {
-            Text(
-                modifier = Modifier.align(Alignment.TopStart),
-                text = product.name
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .padding(10.dp, 0.dp)
+        ) {
+            Column(modifier = Modifier.align(Alignment.CenterStart)) {
+                Text(
+                    text = product.name,
+                    style = Typography.body1
+                )
+                Text(
+                    text = "${product.price}원",
+                    style = Typography.body2
+                )
+            }
+
+            HeartCheckbox(
+                modifier = Modifier.align(Alignment.CenterEnd),
+                checked = checked,
+                onCheckedChange = { checked = it }
             )
         }
     }
@@ -39,12 +61,14 @@ fun ProductItem(
 fun ProductItemPreview() {
     ProductItem(
         product = Product(
-        "pork1",
-        "pork",
-        "돼지고기",
-        24000,
-        "https://android-test.yookgak.com/static/JeongyookgakLogo.png",
-        0
+            "pork1",
+            "pork",
+            "돼지고기",
+            24000,
+            "https://android-test.yookgak.com/static/JeongyookgakLogo.png",
+            0
         )
     )
 }
+
+private val ItemSize = 80.dp
