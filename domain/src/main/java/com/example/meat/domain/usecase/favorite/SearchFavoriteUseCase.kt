aa55631f0ed52d2ed26ffa7bb6d4b5545615d741
양윤재 <diddbswo04@dgsw.hs.kr>
2022-09-17 20:35:01 +0900

    [FIX] FlowOn 추가

--- domain/src/main/java/com/example/meat/domain/usecase/favorite/SearchFavoriteUseCase.kt
+++ domain/src/main/java/com/example/meat/domain/usecase/favorite/SearchFavoriteUseCase.kt
@@ -3,7 +3,9 @@ package com.example.meat.domain.usecase.favorite
 import androidx.paging.PagingData
 import com.example.meat.domain.model.Product
 import com.example.meat.domain.repository.FavoriteRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 
 class SearchFavoriteUseCase(
     private val favoriteRepository: FavoriteRepository
@@ -13,6 +15,6 @@ class SearchFavoriteUseCase(
             favoriteRepository.searchFavorite()
         } else {
             favoriteRepository.searchFavorite(query)
-        }
+        }.flowOn(Dispatchers.IO)
     }
 }
