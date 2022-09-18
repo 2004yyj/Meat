2282412fe9c6148f72d6ab1099be8359c88ebc64
양윤재 <diddbswo04@dgsw.hs.kr>
2022-09-18 14:32:31 +0900

    [ADD] 디테일 UI 구현

--- presentation/src/main/java/com/example/meat/screen/detail/Detail.kt
+++ presentation/src/main/java/com/example/meat/screen/detail/Detail.kt
@@ -3,18 +3,22 @@ package com.example.meat.screen.detail
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.ripple.rememberRipple
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
+import com.example.meat.components.checkbox.HeartCheckbox
 import com.example.meat.drawable.DrawableResources
 
 @Composable
@@ -22,8 +26,10 @@ fun Detail(
     key: String,
     name: String,
     price: Int,
-    onNavigationClick: () -> Unit
+    onNavigationClick: () -> Unit,
 ) {
+    var checked by remember { mutableStateOf(false) }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -50,5 +56,36 @@ fun Detail(
             )
         }
     ) {
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+            ) {
+                Text(
+                    text = name,
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                )
+                Text(
+                    text = "${price}원",
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                )
+                HeartCheckbox(
+                    checked = checked,
+                    onCheckedChange = {
+                        checked = it
+                    },
+                    modifier = Modifier
+                        .padding(top = 10.dp)
+                        .align(Alignment.CenterHorizontally)
+                )
+            }
+        }
     }
 }
