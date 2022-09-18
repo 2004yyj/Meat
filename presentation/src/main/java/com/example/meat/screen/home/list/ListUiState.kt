4621ee00d385265d310bec00823da36255169e28
양윤재 <diddbswo04@dgsw.hs.kr>
2022-09-18 18:50:54 +0900

    [ADD] UiState 파일

---
+++ presentation/src/main/java/com/example/meat/screen/home/list/ListUiState.kt
@@ -0,0 +1,9 @@
+package com.example.meat.screen.home.list
+
+import com.example.meat.domain.model.Product
+
+sealed class ListUiState {
+    object Loading: ListUiState()
+    object Empty: ListUiState()
+    class Success(val value: Map<String, List<Product>>): ListUiState()
+}
