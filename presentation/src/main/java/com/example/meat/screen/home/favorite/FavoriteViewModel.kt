69e05ac4fcd46dd9031bb0bbcc56358611c15d92
양윤재 <diddbswo04@dgsw.hs.kr>
2022-09-18 11:50:17 +0900

    [ADD] 좋아요 상태 업데이트 하는 유스케이스

--- presentation/src/main/java/com/example/meat/screen/home/favorite/FavoriteViewModel.kt
+++ presentation/src/main/java/com/example/meat/screen/home/favorite/FavoriteViewModel.kt
@@ -5,8 +5,10 @@ import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.example.meat.domain.model.Product
+import com.example.meat.domain.usecase.favorite.FavoriteStateChangeUseCase
 import com.example.meat.domain.usecase.favorite.SearchFavoriteUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
@@ -14,7 +16,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class FavoriteViewModel @Inject constructor(
-    private val searchFavoriteUseCase: SearchFavoriteUseCase
+    private val searchFavoriteUseCase: SearchFavoriteUseCase,
+    private val favoriteStateChangeUseCase: FavoriteStateChangeUseCase
 ): ViewModel() {
     private val _product: MutableStateFlow<PagingData<Product>> = MutableStateFlow(PagingData.empty())
     val product = _product.asStateFlow()
@@ -26,6 +29,12 @@ class FavoriteViewModel @Inject constructor(
                 .collect {
                     _product.emit(it)
                 }
+        }
+    }
+
+    fun favoriteStateChange(product: Product) {
+        viewModelScope.launch(Dispatchers.IO) {
+            favoriteStateChangeUseCase(product)
         }
     }
 }
